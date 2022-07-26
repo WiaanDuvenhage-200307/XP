7a69c41539909fa423ed51e26703eca04baca1d3
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-07-26 13:35:15 +0200

    Started with home & category page

--- app/src/main/java/com/example/xp/HomePageActivity.kt
+++ app/src/main/java/com/example/xp/HomePageActivity.kt
@@ -3,9 +3,9 @@ package com.example.xp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) //this sets what XML frontend to use
+        setContentView(R.layout.home_page) //this sets what XML frontend to use
     }
 }
